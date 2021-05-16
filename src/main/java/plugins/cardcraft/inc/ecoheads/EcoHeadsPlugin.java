ed65cc9a3a77629c42a2e8813bd3ed0fcc6d8890
Chris Weible <weiblecr@cardcraft.inc>
2021-05-16 13:21:48 -0600

    CRW - Initial Integration Complete

--- src/main/java/plugins/cardcraft/inc/ecoheads/EcoHeadsPlugin.java
+++ src/main/java/plugins/cardcraft/inc/ecoheads/EcoHeadsPlugin.java
@@ -1,11 +1,16 @@
 package plugins.cardcraft.inc.ecoheads;
 
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.economy.Economy;
 import plugins.cardcraft.inc.ecoheads.listeners.HeadDBListener;
 import plugins.cardcraft.inc.ecoheads.util.Logger;
 
 public class EcoHeadsPlugin extends JavaPlugin {
+	
+	public static Economy ECONOMY = null;
+	
     @Override
     public void onDisable() {
         // Don't log disabling, Spigot does that for you automatically!
@@ -14,10 +19,29 @@ public class EcoHeadsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
-
+        if (!setupEconomy() ) 
+        {
+        	Logger.error(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        
     	Logger.debug("Registering listeners...");
     	new HeadDBListener(this);
 
+    	Logger.debug("Registering commands...");
         getCommand("ecoheads").setExecutor(new EcoHeadsCommand(this));
+    }
+    
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        ECONOMY = rsp.getProvider();
+        return ECONOMY != null;
     }
 }
--- src/main/java/plugins/cardcraft/inc/ecoheads/listeners/HeadDBListener.java
+++ src/main/java/plugins/cardcraft/inc/ecoheads/listeners/HeadDBListener.java
@@ -1,15 +1,21 @@
 package plugins.cardcraft.inc.ecoheads.listeners;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.milkbowl.vault.economy.EconomyResponse;
+import plugins.cardcraft.inc.ecoheads.EcoHeadsPlugin;
 import plugins.cardcraft.inc.ecoheads.util.Logger;
 import tsp.headdb.inventory.PagedPane;
+import tsp.headdb.util.Utils;
 
 
 public class HeadDBListener implements Listener{
@@ -20,21 +26,67 @@ public class HeadDBListener implements Listener{
 
     @EventHandler
     public void onClick(InventoryClickEvent event) {
+    	
+    	if(event == null)
+    	{
+    		return;
+    	}
+    	
         InventoryHolder holder = event.getInventory().getHolder();
-        Inventory eventInventory = event.getInventory();
+        HumanEntity player = event.getWhoClicked();
+        
         //eventInventory.
-        
-        Logger.info("Logging Inventory Event");
-        
+         
         if (holder instanceof PagedPane) {
         	Logger.info("### Entering PagedPane Check ###");
+        	Logger.info("PLAYER: "+player.getName());
+        	if(event.getCurrentItem() == null)
+        	{
+        		return;
+        	}
+        	
         	ItemStack item = event.getCurrentItem();
         	ItemMeta itemMetaData = item.getItemMeta();
+        	String itemMetaString = itemMetaData.toString();
         	
         	Logger.info("EVENT NAME:" + event.getEventName());
         	Logger.info("EVENT ACTION:" + event.getAction());
-        	Logger.info("EVENT ITEM META: " + itemMetaData.toString());
-        	//event.g
+        	Logger.info("EVENT ITEM META: " + itemMetaString);
+        	if(! itemMetaString.contains("\"text\":\"Page \""))
+        	{
+        		Logger.info("DETECTED HEAD SALE!!!!");
+        		double playerBalance = EcoHeadsPlugin.ECONOMY.getBalance(player.getName());
+        		Logger.info("Current Player Balance: " + playerBalance);
+        		Player playerInstance = Bukkit.getPlayer(player.getName());
+        		
+        		if(playerBalance >= 500.0)
+        		{
+            		EconomyResponse response = EcoHeadsPlugin.ECONOMY.withdrawPlayer(player.getName(), 500.0);
+                    if(response.transactionSuccess()) 
+                    {
+                    	Logger.info("Transaction Success, New Balance: "+ EcoHeadsPlugin.ECONOMY.getBalance(player.getName()));                    	
+                    	playerInstance.playSound(playerInstance.getLocation(), Sound.BLOCK_ANVIL_LAND, 1, 100);
+                    } 
+                    else 
+                    {
+                    	Logger.info("Transaction Failed: "+response.errorMessage);
+                    	Utils.sendMessage(playerInstance, "Transaction Failed: " + response.errorMessage);
+                    	playerInstance.playSound(playerInstance.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 0.5f, 100);
+                    	playerInstance.getInventory().remove(item);
+                    	return;
+                    }
+        		}
+        		else
+        		{       
+        			Logger.info("Transaction Failed: Insufficient Account Balance, Heads cost 500 each");
+        			Utils.sendMessage(playerInstance, "Transaction Failed: Insufficient Account Balance");
+        			playerInstance.playSound(playerInstance.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 0.5f, 100);
+        			playerInstance.getInventory().remove(item);
+        		}
+
+
+        	}
+
         	Logger.info("### Exiting PagedPane Check ###");
         }
     }
